30f1aa048b09ae00de7b55bfd0c5ab265b3f4987
De-Jung Tseng <djtseng.c@nycu.edu.tw>
2024-10-21 09:11:09 +0800

    Alter Lesson4_hw.py

--- Lesson_4/Lesson4_hw.py
+++ Lesson_4/Lesson4_hw.py
@@ -7,6 +7,7 @@ class Window(ThemedTk):
         self.title()
         self.title("有夠可愛")
         style=ttk.Style(self)
+        #===================TOP FRAME==================================================================#
         topFrame=ttk.Frame(self,borderwidth=1,relief='groove')
         btn1=ttk.Button(topFrame,text='button1')#在topFrame放進btn1，原本設定的frame寬高就沒了
         btn1.pack(side='left',padx=10,expand=True)#從左向右排
@@ -15,9 +16,11 @@ class Window(ThemedTk):
         btn3=ttk.Button(topFrame,text='button3')
         btn3.pack(side='left',padx=10,expand=True)
         topFrame.pack(ipadx=10,expand=True,fill='x')#空間定位下在frame pack裡面
-
+        #==================End TOP FRAME====================================================#
+        #===================BOTTOM FRAME===============================================================#
         buttomFrame=ttk.Frame(self,borderwidth=1,relief='groove')
-
+        
+        #===================BOTTOM FRAME LEFT===============================================================#
         buttomFrameL=ttk.Frame(self,borderwidth=1,relief='groove')
         btn4=ttk.Button(buttomFrameL,text='button4')
         btn4.pack(side='top',ipady=30,padx=5,pady=(10,5))
@@ -25,8 +28,9 @@ class Window(ThemedTk):
         btn5.pack(side='top',ipady=10,padx=5,pady=5)
         btn6=ttk.Button(buttomFrameL,text='button6')
         btn6.pack(side='top',ipady=10,padx=5,pady=(5,10))
-        buttomFrameL.pack(side='left',expand=True,padx=10)
-
+        buttomFrameL.pack(side='left',expand=True,fill='y',padx=10)
+        #===================End BOTTOM FRAME LEFT===============================================================#
+        #===================BOTTOM FRAME CENTER===============================================================#
         buttomFrameC=ttk.Frame(self,borderwidth=1,relief='groove')
         btn7=ttk.Button(buttomFrameC,text='button7')
         btn7.pack(side='top',ipady=22,padx=(5,8),pady=(5,10))
@@ -34,8 +38,9 @@ class Window(ThemedTk):
         btn8.pack(side='top',ipady=5,padx=(5,8),pady=5)
         btn9=ttk.Button(buttomFrameC,text='button9')
         btn9.pack(side='top',ipady=25,padx=(5,8),pady=5)
-        buttomFrameC.pack(side='left',expand=True,padx=10)
-
+        buttomFrameC.pack(side='left',expand=True,fill='y',padx=10)
+        #===================End BOTTOM FRAME CENTER===============================================================#
+        #===================BOTTOM FRAME RIGHT===============================================================#
         buttomFrameR=ttk.Frame(self,borderwidth=1,relief='groove')
         btn10=ttk.Button(buttomFrameR,text='button9')
         btn10.pack(side='top',ipady=15,padx=5,pady=(0,10))
@@ -43,8 +48,10 @@ class Window(ThemedTk):
         btn11.pack(side='top',ipady=15,padx=5,pady=10)
         btn12=ttk.Button(buttomFrameR,text='button11')
         btn12.pack(side='top',ipady=15,padx=5,pady=10)
-        buttomFrameR.pack(side='left',expand=True,padx=10)
-        buttomFrame.pack(expand=True)
+        buttomFrameR.pack(side='left',expand=True,fill='y',padx=10)
+        #===================End BOTTOM FRAME Right===============================================================#
+        buttomFrame.pack(expand=True,fill='x')
+        #===================End BOTTOM FRAME===============================================================#
 
 def main():
     root=Window(theme='breeze')
---
+++ Lesson_5/Lesson5_0.py
@@ -0,0 +1,94 @@
+from ttkthemes import ThemedTk
+from tkinter import ttk
+
+class Window(ThemedTk):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.title('聽說用字體大小做')
+        style = ttk.Style(self)
+
+        topFrame = ttk.Frame(self,borderwidth=3,relief='groove')
+
+        topFrame.pack(padx=10,pady=(10,0),expand=True,fill='x')
+
+        btn1 = ttk.Button(topFrame,text="按鈕1")
+
+        btn1.pack(side='left',expand=True,fill='x',padx=10)
+
+        btn2 = ttk.Button(topFrame,text="按鈕2")
+
+        btn2.pack(side='left',expand=True,fill='x')
+
+        btn3 = ttk.Button(topFrame,text="按鈕3")
+
+        btn3.pack(side='left',expand=True,fill='x',padx=10)
+
+        topFrame.pack
+
+        #====================TOPFRAME===================================#
+        style.configure('Main1.TButton',font=('Aeial',45))
+        style.configure('Main2.TButton',font=('Aeial',26))
+        style.configure('Main3.TButton',font=('Aeial',25))
+        style.configure('Main4.TButton',font=('Aeial',38))
+        style.configure('Main5.TButton',font=('Aeial',16))
+        style.configure('Main6.TButton',font=('Aeial',38))
+        style.configure('Main7.TButton',font=('Aeial',33))
+        style.configure('Main8.TButton',font=('Aeial',30))
+        style.configure('Main9.TButton',font=('Aeial',34))
+
+        bottomFrame1 = ttk.Frame(self,borderwidth=3,relief='groove')
+
+        bottomFrame1.pack(padx=10,pady=10,side='left')
+        x=3
+        
+        btn4 = ttk.Button(bottomFrame1,text="4",style = 'Main1.TButton',width=x)
+
+        btn4.pack(fill='x',padx=10,pady=(12,0))
+
+        btn5 = ttk.Button(bottomFrame1,text="5",style = 'Main2.TButton',width=x)
+
+        btn5.pack(fill='x',padx=10,pady=(6,0))
+
+        btn6 = ttk.Button(bottomFrame1,text="6",style = 'Main3.TButton',width=x)
+
+        btn6.pack(fill='x',padx=10,pady=(8,12))
+
+        bottomFrame2 = ttk.Frame(self,borderwidth=3,relief='groove')
+
+        bottomFrame2.pack(padx=10,pady=10,side='left')
+
+        btn7 = ttk.Button(bottomFrame2,text="7",style = 'Main4.TButton',width=x)
+
+        btn7.pack(fill='x',padx=10,pady=(12,0))
+
+        btn8 = ttk.Button(bottomFrame2,text="8",style = 'Main5.TButton',width=x)
+
+        btn8.pack(fill='x',padx=10,pady=(14,0))
+
+        btn9 = ttk.Button(bottomFrame2,text="9",style = 'Main6.TButton',width=x)
+
+        btn9.pack(fill='x',padx=10,pady=(8,12))
+
+
+        bottomFrame3 = ttk.Frame(self,borderwidth=3,relief='groove')
+
+        bottomFrame3.pack(padx=10,pady=10)
+
+        btn10 = ttk.Button(bottomFrame3,text="10",style = 'Main7.TButton',width=x)
+
+        btn10.pack(fill='x',padx=10,pady=(0,0))
+
+        btn11 = ttk.Button(bottomFrame3,text="11",style = 'Main8.TButton',width=x)
+
+        btn11.pack(fill='x',padx=10,pady=(8,0))
+
+        btn12 = ttk.Button(bottomFrame3,text="12",style = 'Main9.TButton',width=x)
+
+        btn12.pack(fill='x',padx=10,pady=(16,12))
+def main():
+    window = Window(theme="arc")
+    window.mainloop()
+
+if __name__ == '__main__':
+
+    main()
