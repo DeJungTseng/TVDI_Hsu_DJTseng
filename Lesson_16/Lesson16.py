3158e48b1a21f9954df920c18d277638e07947f7
De-Jung Tseng <djtseng.c@nycu.edu.tw>
2024-12-07 10:50:35 +0800

    flask wtf

--- Lesson_16/Lesson16.py
+++ Lesson_16/Lesson16.py
@@ -1,12 +1,13 @@
 from flask import Flask,render_template,request,redirect,url_for
 import datasource
 from flask_wtf import FlaskForm
-from wtforms import PasswordField
-from wtforms import EmailField,BooleanField,SubmitField
+from wtforms import EmailField,BooleanField,PasswordField,SubmitField
 from wtforms.validators import DataRequired,Length
+import secrets
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY']=secrets.token_hex(16)
 
 # the decorator app make the route to be html format
 @app.route("/")
@@ -41,29 +42,18 @@ def pricing():
                            items_on_page=items_on_page,
                            total_pages=total_pages,
                            page = page)
+
+
 class MyForm(FlaskForm):
-    email_field=EmailField("Email Address",validators=[DataRequired("必須要有資料")])
-    password_field=PasswordField("Email Address",validators=[DataRequired("必須要有資料")],Length(5,10))
-    epaper_field=BooleanField("Subscribe")
-    submit_field=SubmitField("Submit")
+    email_field = EmailField("Email address",validators=[DataRequired("必需要有資料")])
+    password_field = PasswordField("請輸入密碼",validators=[DataRequired("必需要有資料"),Length(5,10)])
+    epaper_field = BooleanField("訂閱電子報")
+    submit_field = SubmitField("確定送出")
     
-
-
 @app.route("/faqs",methods=["GET","POST"])
 def faqs():
-    MyForm=MyForm()
-    if request.method == 'POST':
-        username = request.form['email']
-        password = request.form['password']
-        
-        if username=='cuteeagle@eagle.org'and password=='eagle':
-
-            return redirect(url_for('success'))
-        else:
-            error="ㄚㄚㄚㄚㄚ"
-            return render_template("faqs.j2",error=error)
-        
-    return render_template("faqs.j2")
+    myForm = MyForm()
+    return render_template('faqs.j2',myform = myForm)
 
 @app.route("/about")
 def about():
